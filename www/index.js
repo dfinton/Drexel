4413a6d87c4985818fc39e8731448a8b556c04f1
Dave Finton <dave.finton@gmail.com>
2018-11-17 22:39:28 -0600

    added logic for listening to port

--- www/index.js
+++ www/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/public', express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
