36b85277c0d3a17e138f43e65ed8149086db1aca
Dave Finton <dave.finton@gmail.com>
2018-11-25 15:27:51 -0600

    added bootstrap to project

--- www/index.js
+++ www/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/js/bootstrap', express.static('node_modules/bootstrap/dist/js'));
 app.use('/js/webcomponentsjs', express.static('node_modules/@webcomponents/webcomponentsjs'));
 app.use('/', express.static('public'));
 
