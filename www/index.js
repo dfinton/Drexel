f3b60d8a630b622db0d9e0954d285eb5773e5392
Dave Finton <dave.finton@gmail.com>
2018-11-25 16:00:53 -0600

    added bootstrap for real, also jquery

--- www/index.js
+++ www/index.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// JQuery JS library
+app.use('/js/jquery', express.static('node_modules/jquery/dist'));
+
+// Bootstrap CSS/JS framework (bundled with Popper)
 app.use('/js/bootstrap', express.static('node_modules/bootstrap/dist/js'));
+app.use('/css/bootstrap', express.static('node_modules/bootstrap/dist/css'));
+
+// Web components loader
 app.use('/js/webcomponentsjs', express.static('node_modules/@webcomponents/webcomponentsjs'));
+
+// Static HTML content
 app.use('/', express.static('public'));
 
 app.listen(port, () => {
