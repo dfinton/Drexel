b6e902f18a1729565b75359633c1dba057144a07
Dave Finton <dave.finton@gmail.com>
2018-12-24 16:48:15 -0600

    added requited token in header to api

--- www/api/index.js
+++ www/api/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const dataMiddleware = require('../middleware/data');
 const errorMiddleware = require('../middleware/error');
+const getTokenMiddleware = require('../middleware/get-token');
 const notFoundMiddleware = require('../middleware/not-found');
+const validateTokenMiddleware = require('../middleware/validate-token');
 
 const router = express.Router();
+
+router.use(getTokenMiddleware);
+router.use(validateTokenMiddleware);
 
 router.use('/admin', require('./admin'));
 
