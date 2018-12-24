79be260ccd685e755ccfb6fa25abc03edc55cc6c
Dave Finton <dave.finton@gmail.com>
2018-12-24 17:38:45 -0600

    universe end point now returns universe data

--- www/api/admin/universe/index.js
+++ www/api/admin/universe/index.js
@@ -1,16 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const {
+  InternalServerError,
+} = require('../../../../error');
+
 const router = express.Router();
 
 const Universe = mongoose.model('Universe');
 
+const universeMapper = (datum) => {
+  const {
+    name,
+  } = datum;
+
+  return {
+    name,
+  };
+};
+
 router.get('/', (req, res, next) => {
-  res.locals.data = {
-    Steven: "Universe",
+  const fields = {
+    _id: false,
+    name: true,
   };
 
-  return next();
+  Universe
+    .find()
+    .select(fields)
+    .exec((err, data) => {
+      if (err) {
+        return next(new InternalServerError());
+      }
+
+      const universes = [];
+
+      data.forEach((datum) => {
+        const universe = universeMapper(datum);
+
+        universes.push(universe);
+      });
+
+      res.locals.data = universes;
+
+      return next();
+    });
 });
 
 module.exports = router;
