025cc5d9cc5f8a7dc51cbd0709e19170fa98c671
Dave Finton <dave.finton@gmail.com>
2019-01-01 18:56:38 -0600

    added toolbar buttons for universe management screen

--- client/js/component/admin/universe-list.jsx
+++ client/js/component/admin/universe-list.jsx
@@ -3,6 +3,12 @@ const React = require('react');
 class DrexelAdminUniverseList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.mainAdminMenu = this.mainAdminMenu.bind(this);
+  }
+
+  mainAdminMenu(event) {
+    this.props.resetAdminScreen();
   }
 
   visibleClass() {
@@ -16,7 +22,17 @@ class DrexelAdminUniverseList extends React.Component {
   render() {
     return (
       <div className={this.visibleClass()}>
-        <span>Hello</span>
+        <div className="btn-toolbar" role="toolbar" aria-label="Manage Game Universes">
+          <div className="btn-group mr-2" role="group" aria-label="Create New Game Universe">
+            <button type="button" className="btn btn-primary">Create New Game</button>
+          </div>
+          <div className="btn-group mr-2" role="group" aria-label="Second group">
+            <button type="button" className="btn btn-danger">Delete Selected Games</button>
+          </div>
+          <div className="btn-group" role="group" aria-label="Return to The Main Admin Menu">
+            <button onClick={this.mainAdminMenu} type="button" className="btn btn-secondary">Return to Previous Menu</button>
+          </div>
+        </div>
       </div>
     );
   }
